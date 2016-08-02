ec2ae30da916d14856f54c23184e142a6c14af20
dowdje <dowdje89@gmail.com>
2016-08-02 09:05:53 -0400

    Completed Triangle Lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  attr_reader :kind
+
+  def initialize (a, b, c)
+    a, b, c = [a, b, c].sort
+    if a <= 0 || a + b <= c
+      raise TriangleError
+    elsif a == b && b == c
+      @kind = :equilateral
+    elsif a == b || b == c
+      @kind = :isosceles
+    else
+      @kind = :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+ 
 end
